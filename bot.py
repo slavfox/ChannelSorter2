7ee1ac28c63470ff61d6d686d524c171103a8f4e
Slavfox <slavfoxman@gmail.com>
2022-04-19 11:09:03 +0200

    Documentation, linters

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""/r/ProgrammingLanguages discord channel management bot."""
+import json
 import os
-import traceback
-from datetime import datetime
-from itertools import chain
-from pathlib import Path
+import random
 import re
-from contextlib import redirect_stdout, redirect_stderr
+import traceback
+from contextlib import redirect_stderr, redirect_stdout
+from datetime import datetime
 from io import StringIO
-from typing import List, Dict
-from itertools import combinations
+from itertools import chain, combinations
+from pathlib import Path
+from typing import Dict, List
+from urllib.request import urlopen
 
 import discord
 from discord.ext import commands, tasks
@@ -19,12 +22,21 @@ channels_path = Path(__file__).parent / "categories.txt"
 
 
 class ChannelBot(commands.Bot):
+    """Discordpy bot subclass with convenience methods we need."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.hourly_update.start()
 
     @tasks.loop(hours=1)
     async def hourly_update(self):
+        """Clean up channel list and cycle presence."""
+        await bot.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name="over the project channels",
+            )
+        )
         for guild in bot.guilds:
             log_channel = get_log_channel(guild)
             if log_channel is None:
@@ -32,6 +44,16 @@ class ChannelBot(commands.Bot):
             print(f"Running hourly update in {guild.name}")
             await archive_inactive_inner(guild, log_channel, verbose=True)
             await sort_inner(guild, log_channel, verbose=False)
+        await self.change_presence(
+            activity=discord.Game(name=get_random_top100_steam_game())
+        )
+
+    async def on_ready(self):
+        """Set initial presence."""
+        print(f"Successfully logged in as {self.user}")
+        await self.change_presence(
+            activity=discord.Game(name=get_random_top100_steam_game())
+        )
 
 
 bot = ChannelBot(
@@ -51,7 +73,8 @@ CHANNEL_OWNER_PERMS = discord.PermissionOverwrite(
 )
 
 
-def get_project_categories(guild):
+def get_project_categories(guild) -> List[discord.CategoryChannel]:
+    """Get a list of project categories in the server from the config file."""
     with channels_path.open() as f:
         return [
             discord.utils.get(guild.categories, id=int(line.strip()))
@@ -59,15 +82,21 @@ def get_project_categories(guild):
         ]
 
 
-def get_archive_category(guild):
+def get_archive_category(guild: discord.Guild) -> discord.CategoryChannel:
+    """Get the archive category for the guild."""
     return discord.utils.get(guild.categories, name="Archive")
 
 
-def get_log_channel(guild):
+def get_log_channel(guild: discord.Guild) -> discord.TextChannel:
+    """Return the log channel for the guild."""
     return discord.utils.get(guild.channels, name="channelbot-logs")
 
 
 def unbalancedness(separator_idxs: List[int]) -> int:
+    """
+    Return the sum of squares of differences between sublist lengths given a
+    list of indices of sublist "borders".
+    """
     score = 0
     for prev, nxt in zip(separator_idxs[:-1], separator_idxs[1:]):
         score += (nxt - prev) ** 2
@@ -78,6 +107,13 @@ def balanced_categories(
     categories: List[discord.CategoryChannel],
     channels: List[discord.TextChannel],
 ) -> Dict[int, List[discord.TextChannel]]:
+    """
+    Given a list of categories and channels, return a dict mapping category
+    ID to a list of channels in that category so that the channels are
+    sorted alphabetically, every starting letter is contained entirely
+    within a single category, and each category has a similar number of
+    channels.
+    """
     prev_letter = channels[0].name.upper()[0]
     letter_change_idxs = [0]
     # Save indices with letter changes
@@ -111,6 +147,7 @@ def balanced_categories(
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def get_categories(ctx):
+    """Get the names of the project categories."""
     await ctx.send(
         f"Project categories: "
         f"{[c.name for c in get_project_categories(ctx.guild)]}"
@@ -118,8 +155,9 @@ async def get_categories(ctx):
 
 
 @bot.command()
-@commands.has_permissions(administrator=True)
+@commands.is_owner()
 async def set_categories(ctx, *categories: discord.CategoryChannel):
+    """Set the ids of the project categories."""
     with channels_path.open("w") as f:
         for category in categories:
             f.write(str(category.id) + "\n")
@@ -138,6 +176,7 @@ async def sort_inner(
     log_channel: discord.TextChannel,
     verbose: bool = True,
 ):
+    """Channel sorting logic."""
     if verbose:
         await log_channel.send("Sorting channels!")
 
@@ -210,7 +249,9 @@ async def sort_inner(
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def make_channel(ctx, owner: discord.Member, name: str):
+    """Create a new project channel and role."""
     new_channel = await ctx.guild.create_text_channel(name=name)
+    await reposition_channel(new_channel, get_project_categories(ctx.guild))
     await ctx.send(f"Created channel {new_channel.mention}.")
     role = await ctx.guild.create_role(
         name=f"lang: {name.capitalize()}",
@@ -231,24 +272,10 @@ async def make_channel(ctx, owner: discord.Member, name: str):
             send_messages=False, add_reactions=False
         ),
     }
-    categories = get_project_categories(ctx.guild)
-    channels = []
-    for cat in categories:
-        channels.extend(cat.channels)
-    channels = sorted(channels, key=lambda channel: channel.name)
-    position = len(categories[-1].channels)
-    category = categories[-1]
-    for channel in channels:
-        if channel.name.lower() > name.lower():
-            position = channel.position
-            category = channel.category
-            break
-    await new_channel.edit(
-        category=category, position=position, overwrites=overwrites
-    )
-    await ctx.send(
-        f"Set appropriate permissions for {new_channel.mention}. Done!"
-    )
+    await new_channel.edit(overwrites=overwrites)
+    await ctx.send(f"Set appropriate permissions for {new_channel.mention}.")
+    await sort_inner(ctx.guild, ctx.channel, verbose=True)
+    await ctx.send(f"Done!")
 
 
 @bot.command()
@@ -256,7 +283,7 @@ async def make_channel(ctx, owner: discord.Member, name: str):
 async def archive(ctx):
     """Archive a channel."""
     await ctx.send("Archiving channel.")
-    get_log_channel(ctx.guild).send(
+    await get_log_channel(ctx.guild).send(
         f"Channel {ctx.channel.mention} archived manually by owner."
     )
     await ctx.channel.edit(category=get_archive_category(ctx.guild))
@@ -282,8 +309,9 @@ async def archive_inactive_inner(
     log_channel: discord.TextChannel,
     verbose: bool = True,
 ):
+    """Archive project channels that have been inactive for over 90 days."""
     if verbose:
-        await log_channel.send(f"Archiving inactive project channels.")
+        await log_channel.send("Archiving inactive project channels.")
     archived = 0
     channel: discord.TextChannel
     for channel in chain.from_iterable(
@@ -324,10 +352,10 @@ async def archive_inactive_inner(
 async def run_python(ctx, *, code):
     """Run arbitrary Python."""
 
-    async def aexec(code, globals_, locals_):
+    async def aexec(source, globals_, locals_):
         exec(
-            f"async def __ex(ctx, globals, locals): "
-            + "".join(f"\n {l}" for l in code.split("\n")),
+            "async def __ex(ctx, globals, locals): "
+            + "".join(f"\n {line}" for line in source.split("\n")),
             globals_,
             locals_,
         )
@@ -347,10 +375,10 @@ async def run_python(ctx, *, code):
 
 
 @bot.listen("on_message")
-async def on_message(message: discord.Message):
+async def on_message(message: discord.Message) -> None:
     """Listen for messages in archived channels to unarchive them."""
     if (
-        message.guild is None
+        not isinstance(message.guild, discord.Guild)
         or message.channel.category is None
         or message.channel.category != get_archive_category(message.guild)
     ):
@@ -364,6 +392,10 @@ async def on_message(message: discord.Message):
 
 
 async def reposition_channel(channel, project_categories):
+    """
+    Try to position a channel where it should be in the projects
+    categories without resorting everything.
+    """
     channels = sorted(
         (
             ch
@@ -391,6 +423,7 @@ async def reposition_channel(channel, project_categories):
 
 @bot.event
 async def on_command_error(ctx, error):
+    """Handle command errors by sending the stringified exception back."""
     traceback.print_exception(error)
     await ctx.reply(str(error))
 
@@ -403,10 +436,20 @@ async def on_guild_channel_update(before, after):
     categories = get_project_categories(before.guild)
     if not (after.category in categories and after.name != before.name):
         return
-    get_log_channel(before.guild).send(
+    await get_log_channel(before.guild).send(
         f"Channel {before.mention} was renamed: {before.name} -> {after.name}"
     )
     await reposition_channel(after, categories)
+
+
+# Fun stuff
+def get_random_top100_steam_game() -> str:
+    """Get the name of a random top 100 steam game."""
+    games = json.load(
+        urlopen("https://steamspy.com/api.php?request=top100in2weeks")
+    )
+    game = random.choice(list(games.keys()))
+    return games[game]["name"]
 
 
 bot.run(os.getenv("CHANNELSORTER_TOKEN"))
